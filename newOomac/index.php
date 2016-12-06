4bf58ce65a7b5c8f3ce86d540b8650b9a88a3418
Ayandiran Mayomi <ayandiranmayomi@gmail.com>
2016-12-06 10:53:28 +0000

    Update index.php

--- newOomac/index.php
+++ newOomac/index.php
@@ -175,7 +175,7 @@
 <?php } ?>
 			</div>
 			<div class="col-md-6 oaks">
-				<h3>SEVENOAKS</h3>
+				<h3>Oyejide Oloyede Memorial Anglican Church(OOMAC)</h3>
 				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
 				<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 				<a class="read" href="#">READ MORE >></a>
